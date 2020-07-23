81579814660dbbe0cb361b6995cc96c24672dd85
ERIC WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGER3@OOCL.COM>
2020-07-23 20:09:09 +0800

    [game][Eric] featrue: pass should_return_false_when_guess_valid_given_1122

--- src/main/java/tw/eric/game/GuessValid.java
+++ src/main/java/tw/eric/game/GuessValid.java
@@ -5,6 +5,14 @@ public class GuessValid {
         if (inputGuess.length != 4) {
             return false;
         }
+        int sum = 0;
+        int n = inputGuess.length;
+        for(int i : inputGuess) {
+            sum += i;
+        }
+        if (sum != n*(n+1)/2) {
+            return false;
+        }
         for (int i = 0; i < inputGuess.length; i++) {
             if (inputGuess[i] < 0 || inputGuess[i] > 9) {
                 return false;
