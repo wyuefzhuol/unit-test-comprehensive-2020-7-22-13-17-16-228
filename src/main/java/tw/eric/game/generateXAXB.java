bafc27d0a33a4b5ce9ce5b86babc1bbfd6d35054
ERIC WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGER3@OOCL.COM>
2020-07-23 18:57:27 +0800

    [game][Eric] featrue: pass should_return_0A0B_when_guess_given_5678

--- src/main/java/tw/eric/game/generateXAXB.java
+++ src/main/java/tw/eric/game/generateXAXB.java
@@ -2,8 +2,18 @@ package tw.eric.game;
 
 public class generateXAXB {
     public String guess(int[] answer, int[] inputGuess) {
+        boolean flag = false;
         for (int i = 0; i < inputGuess.length; i++) {
-            if (inputGuess[i] != answer[i]) {
+            for (int j = 0; j < answer.length; j++) {
+                if (inputGuess[i] == answer[j]) {
+                    flag = true;
+                    break;
+                }
+                if (i == inputGuess.length-1 && j == answer.length-1) {
+                    return "0A0B";
+                }
+            }
+            if (flag && inputGuess[i] != answer[i]) {
                 return null;
             }
         }
