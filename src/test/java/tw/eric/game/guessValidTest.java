e0cc91d79fa906ecff78b1ada81007d5d7f18c00
ERIC WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGER3@OOCL.COM>
2020-07-23 20:04:05 +0800

    [game][Eric] test: should_return_false_when_guess_valid_given_1122

--- src/test/java/tw/eric/game/guessValidTest.java
+++ src/test/java/tw/eric/game/guessValidTest.java
@@ -43,4 +43,17 @@ public class guessValidTest {
         //then
         assertEquals(false, isValid);
     }
+
+    @Test
+    void should_return_false_when_guess_valid_given_1122() {
+        //given
+        GuessValid guessValid = new GuessValid();
+        int[] inputGuess = {1,1,2,2};
+
+        //when
+        boolean isValid = guessValid.isGuessValid(inputGuess);
+
+        //then
+        assertEquals(false, isValid);
+    }
 }
