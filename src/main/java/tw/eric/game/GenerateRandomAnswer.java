2da3828918fb90e596e1711516201b81df606527
ERIC WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGER3@OOCL.COM>
2020-07-23 20:40:22 +0800

    [game][Eric] fix-bug: fix the generate random answer bug

--- src/main/java/tw/eric/game/GenerateRandomAnswer.java
+++ src/main/java/tw/eric/game/GenerateRandomAnswer.java
@@ -3,21 +3,37 @@ package tw.eric.game;
 public class GenerateRandomAnswer implements GenerateAnswer {
     @Override
     public int[] generateAnswer() {
-        int[] answer = {1,2,3,4};
-        return answer;
+        int[] result = new int[4];
+        int count = 0;
+        while (count < 4) {
+            int num = (int) (Math.random() * 9);
+            boolean flag = true;
+            for (int j = 0; j < 4; j++) {
+                if (num == result[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                result[count] = num;
+                count++;
+            }
+        }
+        return result;
     }
 
     public boolean isGuessValid(int[] answer) {
         if (answer.length != 4) {
             return false;
         }
-        int sum = 0;
-        int n = answer.length;
-        for(int i : answer) {
-            sum += i;
-        }
-        if (sum != n*(n+1)/2) {
-            return false;
+        int temp = 0;
+        for (int i = 0; i < answer.length - 1; i++) {
+            temp = answer[i];
+            for (int j = i + 1; j < answer.length; j++) {
+                if (temp == answer[j]) {
+                    return false;
+                }
+            }
         }
         for (int i = 0; i < answer.length; i++) {
             if (answer[i] < 0 || answer[i] > 9) {
