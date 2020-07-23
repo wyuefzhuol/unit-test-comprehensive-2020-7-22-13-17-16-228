12c4489c01790a9fa2044d251d5b7274d8a44619
ERIC WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGER3@OOCL.COM>
2020-07-23 18:24:30 +0800

    [game][Eric] test: should_return_4A0B_when_guess_given_1234

---
+++ src/main/java/tw/eric/game/generateXAXB.java
@@ -0,0 +1,7 @@
+package tw.eric.game;
+
+public class generateXAXB {
+    public String guess(int[] inputGuess) {
+        return null;
+    }
+}
---
+++ src/test/java/tw/eric/game/generateXAXBTest.java
@@ -0,0 +1,21 @@
+package tw.eric.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class generateXAXBTest {
+    @Test
+    void should_return_4A0B_when_guess_given_1234() {
+        //given
+        generateXAXB generatexAxB = new generateXAXB();
+        int[] answer = {1, 2, 3, 4};
+        int[] inputGuess = {1,2,3,4};
+
+        //when
+        String result = generatexAxB.guess(inputGuess);
+
+        //then
+        assertEquals("4A0B",result);
+    }
+}
