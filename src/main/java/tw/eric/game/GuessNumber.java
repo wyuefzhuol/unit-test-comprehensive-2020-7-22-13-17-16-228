47b875cf90447e03a2214e5215b95d0768295993
wyuefzhuol <45565676+wyuefzhuol@users.noreply.github.com>
2020-07-27 20:40:01 +0800

    [Guess-number][Eric] refactor: delete the class GuessValid and put it into class GuessNumber

--- src/main/java/tw/eric/game/GuessNumber.java
+++ src/main/java/tw/eric/game/GuessNumber.java
@@ -16,16 +16,15 @@ public class GuessNumber {
     public int[] inputGuess() {
         int[] guess = new int[4];
         Scanner sc = new Scanner(System.in);
-        GuessValid gv = new GuessValid();
         do {
             System.out.println("Please input your guess number");
             for(int i = 0; i < guess.length; i++){
                 guess[i] = sc.nextInt();
             }
-            if (!gv.isGuessValid(guess)) {
+            if (!isGuessValid(guess)) {
                 System.out.println("Not valid, please input again");
             }
-        } while (!gv.isGuessValid(guess));
+        } while (!isGuessValid(guess));
         return guess;
     }
 
@@ -48,5 +47,26 @@ public class GuessNumber {
     public boolean isGameWin(int[] guess) {
         System.out.println("Output: " + getGuessResult(guess));
         return getGuessResult(guess).equals("4A0B");
+    }
+
+    public boolean isGuessValid(int[] inputGuess) {
+        if (inputGuess.length != 4) {
+            return false;
+        }
+        int temp = 0;
+        for (int i = 0; i < inputGuess.length - 1; i++) {
+            temp = inputGuess[i];
+            for (int j = i + 1; j < inputGuess.length; j++) {
+                if (temp == inputGuess[j]) {
+                    return false;
+                }
+            }
+        }
+        for (int i = 0; i < inputGuess.length; i++) {
+            if (inputGuess[i] < 0 || inputGuess[i] > 9) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/tw/eric/game/guessValidTest.java
+++ src/test/java/tw/eric/game/guessValidTest.java
@@ -3,16 +3,21 @@ package tw.eric.game;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class guessValidTest {
     @Test
     void should_return_true_when_guess_valid_given_1234() {
         //given
-        GuessValid guessValid = new GuessValid();
+        int[] answer = {1,2,3,4};
+        GenerateAnswer generateAnswer = mock(GenerateAnswer.class);
+        when(generateAnswer.generateAnswer()).thenReturn(answer);
+        GuessNumber guessNumber = new GuessNumber(generateAnswer);
         int[] inputGuess = {1,2,3,4};
 
         //when
-        boolean isValid = guessValid.isGuessValid(inputGuess);
+        boolean isValid = guessNumber.isGuessValid(inputGuess);
 
         //then
         assertEquals(true, isValid);
@@ -21,11 +26,14 @@ public class guessValidTest {
     @Test
     void should_return_false_when_guess_valid_given_112314() {
         //given
-        GuessValid guessValid = new GuessValid();
+        int[] answer = {1,2,3,4};
+        GenerateAnswer generateAnswer = mock(GenerateAnswer.class);
+        when(generateAnswer.generateAnswer()).thenReturn(answer);
+        GuessNumber guessNumber = new GuessNumber(generateAnswer);
         int[] inputGuess = {1,12,3,14};
 
         //when
-        boolean isValid = guessValid.isGuessValid(inputGuess);
+        boolean isValid = guessNumber.isGuessValid(inputGuess);
 
         //then
         assertEquals(false, isValid);
@@ -34,11 +42,14 @@ public class guessValidTest {
     @Test
     void should_return_false_when_guess_valid_given_12() {
         //given
-        GuessValid guessValid = new GuessValid();
+        int[] answer = {1,2,3,4};
+        GenerateAnswer generateAnswer = mock(GenerateAnswer.class);
+        when(generateAnswer.generateAnswer()).thenReturn(answer);
+        GuessNumber guessNumber = new GuessNumber(generateAnswer);
         int[] inputGuess = {1,2};
 
         //when
-        boolean isValid = guessValid.isGuessValid(inputGuess);
+        boolean isValid = guessNumber.isGuessValid(inputGuess);
 
         //then
         assertEquals(false, isValid);
@@ -47,11 +58,14 @@ public class guessValidTest {
     @Test
     void should_return_false_when_guess_valid_given_1122() {
         //given
-        GuessValid guessValid = new GuessValid();
+        int[] answer = {1,2,3,4};
+        GenerateAnswer generateAnswer = mock(GenerateAnswer.class);
+        when(generateAnswer.generateAnswer()).thenReturn(answer);
+        GuessNumber guessNumber = new GuessNumber(generateAnswer);
         int[] inputGuess = {1,1,2,2};
 
         //when
-        boolean isValid = guessValid.isGuessValid(inputGuess);
+        boolean isValid = guessNumber.isGuessValid(inputGuess);
 
         //then
         assertEquals(false, isValid);
