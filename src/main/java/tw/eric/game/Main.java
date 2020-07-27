ccc2665704e0e685a90778ddd9e9498af47cd45e
wyuefzhuol <45565676+wyuefzhuol@users.noreply.github.com>
2020-07-27 20:25:01 +0800

    [Guess-number][Eric] refactor: Define magic numbers in the Main.main as constants

--- src/main/java/tw/eric/game/Main.java
+++ src/main/java/tw/eric/game/Main.java
@@ -1,6 +1,8 @@
 package tw.eric.game;
 
 public class Main {
+    final public static String winMessage = "You win!!";
+    final public static String failedMessage = "You failes!!";
 
     public static void main(String[] args) {
         GenerateRandomAnswer generateRandomAnswer = new GenerateRandomAnswer();
@@ -8,13 +10,15 @@ public class Main {
 
         GuessNumber GuessNumber = new GuessNumber(answer);
 
-        for (int roundNumber = 0; roundNumber < 6; roundNumber++) {
+        final int firstRound = 1;
+        final int lastRound = 6;
+        for (int roundNumber = firstRound; roundNumber <= lastRound; roundNumber++) {
             int[] guess = GuessNumber.inputGuess();
             if (GuessNumber.isGameWin(guess)) {
-                System.out.println("You win!!");
+                System.out.println(winMessage);
             }
-            if (roundNumber == 5) {
-                System.out.println("You failed!!");
+            if (roundNumber == lastRound) {
+                System.out.println(failedMessage);
             }
         }
     }
