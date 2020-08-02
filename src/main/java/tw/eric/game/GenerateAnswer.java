5ed7a477c5b4eae4f60fb980d58b7f3fb6d00ab7
wyuefzhuol <45565676+wyuefzhuol@users.noreply.github.com>
2020-08-02 20:21:48 +0800

    [Guess-number][Eric] test: should_return_answer_1234_when_generate_answer_given_random_number_1234

--- src/main/java/tw/eric/game/GenerateAnswer.java
+++ src/main/java/tw/eric/game/GenerateAnswer.java
@@ -3,22 +3,6 @@ package tw.eric.game;
 public class GenerateAnswer {
 
     public int[] generateAnswer() {
-        int[] result = new int[4];
-        int count = 0;
-        while (count < 4) {
-            int num = (int) (Math.random() * 9);
-            boolean flag = true;
-            for (int j = 0; j < 4; j++) {
-                if (num == result[j]) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                result[count] = num;
-                count++;
-            }
-        }
-        return result;
+        return null;
     }
 }
--- src/test/java/tw/eric/game/generateAnswerTest.java
+++ src/test/java/tw/eric/game/generateAnswerTest.java
@@ -2,8 +2,27 @@ package tw.eric.game;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class generateAnswerTest {
 
+    @Test
+    void should_return_answer_1234_when_generate_answer_given_random_number_1234() {
+        //given
+        int[] randomNumber = {1,2,3,4};
+        GenerateAnswer generateAnswer = mock(GenerateAnswer.class);
+        when(new Random().ints(0, 9).distinct().limit(4).toArray())
+                .thenReturn(randomNumber);
+        GenerateAnswer generatingAnswer = new GenerateAnswer();
+
+        //when
+        int[] answer = generatingAnswer.generateAnswer();
+
+        //then
+        assertEquals(randomNumber, answer);
+    }
 }
